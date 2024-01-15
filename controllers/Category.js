883fbe383a5df35fd34b41ca4f1063404c42c3a5
Harshit chopde <109473586+Harshitchopde@users.noreply.github.com>
2024-01-15 18:24:11 +0530

    Update controller

--- controllers/Category.js
+++ controllers/Category.js
@@ -2,92 +2,94 @@ const Category = require("../models/Category");
 const Course = require("../models/Course");
 
 
-exports.createCategory = async(req,res)=>{
+exports.createCategory = async (req, res) => {
     try {
-        const {name ,desc} = req.body;
+        const { name, desc } = req.body;
         // validation 
-        if(!name || !desc){
+        if (!name || !desc) {
             return res.status(400).json({
-                status:false,
-                message:"Some field is missing name or desc",
+                status: false,
+                message: "Some field is missing name or desc",
             })
         }
         // create entry in db 
         const tagEntry = await Category.create({
-            name:name,
-            desc:desc,
+            name: name,
+            desc: desc,
         })
         return res.status(200).json({
-            status:true,
-            message:"Successfull Created Category",
+            status: true,
+            message: "Successfull Created Category",
             tagEntry
         })
 
     } catch (error) {
-        console.log("Error in createCategory : ",error.message);
+        console.log("Error in createCategory : ", error.message);
         return res.status(400).json({
-            status:false,
-            message:error.message,
+            status: false,
+            message: error.message,
         })
-        
+
     }
 }
-exports.showAllCategory =async (req,res)=>{
+exports.showAllCategory = async (req, res) => {
     try {
-        const getAllTags = await Category.find({},{name:true,desc:true});// make sure name and desc is comming
+        const getAllTags = await Category.find({}, { name: true, desc: true });// make sure name and desc is comming
         return res.status(200).json({
-            success:true,
-            message:"All Category successfully fetched",
+            success: true,
+            message: "All Category successfully fetched",
         })
     } catch (error) {
         return res.status(400).json({
-            status:false,
-            message:error.message,
+            status: false,
+            message: error.message,
         })
     }
 }
 
 // getCategoryPageDetails 
-exports.categoryPageDetails = async(req,res)=>{
+exports.categoryPageDetails = async (req, res) => {
     try {
         // get categoryId 
-        const {categoryId} = req.body;
+        const { categoryId } = req.body;
         // get course for particular categorys
         const getCategory = await Category.find(
-                                   {_id:{$e:categoryId}}
-                                   );
+            { _id: { $e: categoryId } }
+        ).populate("courses").exec();
         // validate
-        if(!getCategory){
+        if (!getCategory) {
             return res.status(404).json({
-                success:false,
-                message:"Category not Found!"
+                success: false,
+                message: "Category not Found!"
             })
         }
         // get Courses for different categorys course
-        const diffCategory = await Category.find({_id:{$ne:categoryId}});
-        if(!diffCategory){
+        const diffCategory = await Category.find({ _id: { $ne: categoryId } }).populate("courses").exec();
+        if (!diffCategory) {
             return res.status(404).json({
-                success:false,
-                message:"Other Category not Found!"
+                success: false,
+                message: "Other Category not Found!"
             })
         }
         // get top selling courses
-        const topSellingCourse = await Course.find({}).sort({purchase:-1});
+        const topSellingCourse = await Course.find({}).sort({ purchase: -1 });
         // return response 
         return res.status(200).json({
-            success:true,
-            category:getCategory,
-            differentCategory:diffCategory,
-            topSellingCourse, 
+            success: true,
+            data: {
+                category: getCategory,
+                differentCategory: diffCategory,
+                topSellingCourse,
+            }
 
         })
     } catch (error) {
-        console.log("Error in category Page Details ",error.message);
-        
-        res.status(400).json({
-            success:false,
-            message:error.message,
+        console.log("Error in category Page Details ", error.message);
+
+        res.status(500).json({
+            success: false,
+            message: error.message,
         })
-        
+
     }
 }
