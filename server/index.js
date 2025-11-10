5e1cd8d32fffc3fa1cf8283fc5b99bab7ba4e20b
Harshitchopde <chopdeharshit@gmail.com>
2025-11-10 19:30:17 +0530

    added the frontend url to cors

--- server/index.js
+++ server/index.js
@@ -20,7 +20,10 @@ app.use(json())
 app.use(cookieParser())
 app.use(cors(
     {
-        origin:"http://localhost:3000",
+        origin:["http://localhost:3000",
+            "https://model-ed-tech-platfrom.vercel.app"
+        ]
+        ,
         credentials:true,
     }
 ))
